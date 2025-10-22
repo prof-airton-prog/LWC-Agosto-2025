ba16addfbee320afbb6a789f8f2f121ed787367a
Airton Miguel Junior <airton.migueljunior@gmail.com>
2025-10-21 21:04:01 -0300

    Lifecycle

---
+++ force-app/main/default/lwc/lifecycle_2/lifecycle_2.js
@@ -0,0 +1,40 @@
+import { LightningElement } from 'lwc';
+
+export default class Lifecycle_2 extends LightningElement {
+    getCurrentTimestampWithMilliseconds()
+    {
+        const now = new Date();
+
+        const year = now.getFullYear();
+        const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed
+        const day = now.getDate().toString().padStart(2, '0');
+
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+        const milliseconds = now.getMilliseconds().toString().padStart(3, '0'); // Pad with '0' to ensure 3 digits
+
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+    }
+    
+    constructor()
+    {
+        super();
+        console.log('Constructor do filho - '+ this.getCurrentTimestampWithMilliseconds());
+    }
+
+    connectedCallback()
+    {
+        console.log('connectedCallback do filho - '+ this.getCurrentTimestampWithMilliseconds());
+    }
+
+    disconnectedCallback()
+    {
+        console.log('disconnectedCallback do filho - '+ this.getCurrentTimestampWithMilliseconds());
+    }
+
+    renderedCallback()
+    {
+        console.log('renderedCallback do filho - '+ this.getCurrentTimestampWithMilliseconds());
+    }
+}
