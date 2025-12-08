6b60eaeb45d3c2d98894e84c2cd292bf861e1877
Airton Miguel Junior <airton.migueljunior@gmail.com>
2025-12-08 19:31:58 -0300

    Mixin3 - navegação entre páginas (aba custom) com link de doc salesforce

---
+++ force-app/main/default/lwc/navegaEntreTabs/navegaEntreTabs.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class NavegaEntreTabs extends NavigationMixin(LightningElement) {
+
+    openCustomTab(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__navItemPage',
+            attributes: {
+                apiName: 'Welcome' //Onde vai o nome da Aba
+            }
+        });
+    }
+}
+
+
+//https://developer.salesforce.com/docs/platform/lwc/guide/use-navigate-page-types.html
